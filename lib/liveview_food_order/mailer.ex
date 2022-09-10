6019b9a9cf902c7a122066559eeb84cd97e0ada9
Guto Meirelles <gutojm@gmail.com>
2022-09-10 15:06:00 -0300

    Testing GitHub ci (#1)
    * more configuration files
    
    * fixing credo issues
    
    * fixing format issues

--- lib/liveview_food_order/mailer.ex
+++ lib/liveview_food_order/mailer.ex
@@ -1,3 +1,5 @@
 defmodule LiveviewFoodOrder.Mailer do
+  @moduledoc false
+
   use Swoosh.Mailer, otp_app: :liveview_food_order
 end
--- lib/liveview_food_order_web/telemetry.ex
+++ lib/liveview_food_order_web/telemetry.ex
@@ -1,4 +1,6 @@
 defmodule LiveviewFoodOrderWeb.Telemetry do
+  @moduledoc false
+
   use Supervisor
   import Telemetry.Metrics
 
