82671d1745d405a7b76500dba392eb1524711ca3
guto <gutojm@gmail.com>
2022-10-08 15:48:06 -0300

    criando componentes

--- lib/liveview_food_order_web/live/main/main_live.ex
+++ lib/liveview_food_order_web/live/main/main_live.ex
@@ -1,5 +1,7 @@
 defmodule LiveviewFoodOrderWeb.MainLive do
   use LiveviewFoodOrderWeb, :live_view
+  alias LiveviewFoodOrderWeb.Main.Hero
+  alias LiveviewFoodOrderWeb.Main.Items
 
   def mount(_assigns, _session, socket) do
     {:ok, socket |> assign(name: "Guto")}
